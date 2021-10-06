5e1afef9a9729ec6a5b075a8912cfdb48ccd8efe
beGuided <adejoh382@gmail.com>
2021-10-06 17:47:24 +0100

    update

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,6 +1,16 @@
 <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','Adejoh382@');
-define('DB_NAME','Email_system');
+// heroku login
+
+define('DB_HOST','us-cdbr-east-04.cleardb.com');
+define('DB_USER','bcab774f954db0');
+define('DB_PASS','5e237746');
+define('DB_NAME','heroku_139732e5ff8d1fd');
+
+
+
+// define('DB_HOST','localhost');
+// define('DB_USER','root');
+// define('DB_PASS','');
+// define('DB_NAME','Email_system');
+
